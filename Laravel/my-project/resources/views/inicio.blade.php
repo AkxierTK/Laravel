8cd2dad2c9ca534271fb1ee560205c7b2e64ad90
riberapelaez@gmail.com <riberapelaez@gmail.com>
2022-02-01 16:19:34 +0100

    git

--- Laravel/my-project/resources/views/inicio.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('plantilla')
-
-@section('apartado')
-<header>
-  <h2>Inicio</h2>
-</header>
-@endsection
--- Laravel/my-project/resources/views/proyecto.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('plantilla')
-
-@section('apartado')
-<header>
-  <h2>Estas en el proyecto</h2>
-</header>
-@endsection
